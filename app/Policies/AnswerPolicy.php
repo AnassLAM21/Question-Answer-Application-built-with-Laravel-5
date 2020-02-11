27235151561b45061353785a1e862fe33d80bd68
anasslam21 <anasslam69@gmail.com>
2020-02-11 01:09:51 +0100

    merge lesson-20

--- app/Policies/AnswerPolicy.php
+++ app/Policies/AnswerPolicy.php
@@ -8,12 +8,8 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class AnswerPolicy
 {
-<<<<<<< HEAD
-    use HandlesAuthorization;    
-=======
     use HandlesAuthorization;
 
->>>>>>> c7344d8f3ba6e1d1dd332aec07906f847ea73c62
 
     /**
      * Determine whether the user can update the answer.
@@ -24,7 +20,6 @@ class AnswerPolicy
      */
     public function update(User $user, Answer $answer)
     {
-<<<<<<< HEAD
         return $user->id === $answer->user_id;
     }
 
@@ -35,55 +30,13 @@ class AnswerPolicy
 
     /**
      * Determine whether the user can delete the answer.
-=======
-        return $user->id == $answer->user_id;
-    }
-
-    public function accept(User $user, Answer $answer)
-    {
-        return $user->id == $answer->question->user_id;
-    }
-
-    /**
-     * Determine whether the user can delete the answer.
      *
      * @param  \App\User  $user
      * @param  \App\Answer  $answer
      * @return mixed
      */
-    public function delete(User $user, Answer $answer)
-    {
-        return $user->id == $answer->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the answer.
->>>>>>> c7344d8f3ba6e1d1dd332aec07906f847ea73c62
-     *
-     * @param  \App\User  $user
-     * @param  \App\Answer  $answer
-     * @return mixed
-     */
-<<<<<<< HEAD
     public function delete(User $user, Answer $answer)
     {
         return $user->id === $answer->user_id;
-=======
-    public function restore(User $user, Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the answer.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Answer  $answer
-     * @return mixed
-     */
-    public function forceDelete(User $user, Answer $answer)
-    {
-        //
->>>>>>> c7344d8f3ba6e1d1dd332aec07906f847ea73c62
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
-Route::resource('questions', 'QuestionsController')->except('show');
-// Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
-Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
-Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
-
-Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept');
-=======
-Route::resource('questions','QuestionsController')->except('show');
-//Route::post('/questions/{question}/answers','AnswersController@store')->name('answer.store');
-Route::resource('questions.answers','AnswersController')->except(['index','create','show']);
-Route::get('/questions/{slug}','QuestionsController@show')->name('questions.show');
-<<<<<<< HEAD
 ?>
->>>>>>> c7344d8f3ba6e1d1dd332aec07906f847ea73c62
-=======
-Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept');
-
-
-?>
->>>>>>> lesson-20
